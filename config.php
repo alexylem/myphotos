de606548be566036263eedcd982904e957a603c0
Alexandre Mély <alexandre.mely@gmail.com>
2015-02-15 17:07:37 +0100

    Update config.php

--- config.php
+++ config.php
@@ -7,7 +7,7 @@ $config = array (
 	// Google API Client Id (https://console.developers.google.com/project)
 	'client_id'			=> '320030776284-01dr10ur7ni4opbjgum9g266vrn29u4f.apps.googleusercontent.com',
 	// Google API Client secret
-	'client_secret'		=> 'nHnWoauT2Ri51upqJOzKmKi0',
+	'client_secret'		=> '',
 	// System path to original photo directory are stored (/path/to/photos/)
 	'photopath'			=> '/Users/alex/Documents/Dev Web/www/myphotos/photos/',
 	// URL to the same photo directory (http://localhost/photos/)
