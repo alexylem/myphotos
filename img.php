959cd2bd1782fb1da1d543b804dbac02e89ce4a2
Alexandre Mély <alexandre.mely@gmail.com>
2015-02-21 16:40:18 +0100

    browser cache support

--- img.php
+++ img.php
@@ -1,21 +1,26 @@
 <?php
 ini_set('display_errors', 1);
-header('Access-Control-Allow-Origin: *');
+//header('Access-Control-Allow-Origin: *'); // only for WS
 
 // Settings
-include_once ('config.php');
+include ('config.php');
+include ('utils.php');
 
 $file = $config['photopath'].$_GET['f'];
 
-// Set content type
-$types = Array(
-	'1'  => 'image/gif',
-    '2'  => 'image/jpeg',
-    '3'  => 'image/png',
-    '6'  => 'image/bmp'
-);
-header('Content-Type: '.$types[exif_imagetype($file)]);
+$filemtime = filemtime($file);
+$gmt_mtime = gmdate('r', $filemtime);
+header('ETag: "'.md5($filemtime.$file).'"');
+header('Last-Modified: '.$gmt_mtime);
+header('Cache-Control: public, max-age=31536000'); 
 
-// Output file content
-readfile($file);
+if(isset($_SERVER['HTTP_IF_MODIFIED_SINCE']) || isset($_SERVER['HTTP_IF_NONE_MATCH'])) {
+    if ($_SERVER['HTTP_IF_MODIFIED_SINCE'] == $gmt_mtime || str_replace('"', '', stripslashes($_SERVER['HTTP_IF_NONE_MATCH'])) == md5($filemtime.$file)) {
+        header('HTTP/1.1 304 Not Modified');
+        exit();
+    }
+}
+
+header('Content-type: '.mime_type_from_ext ($file));
+readfile($file); 
 ?>
