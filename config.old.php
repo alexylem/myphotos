616765f904bc40675c6642dccec867e759ea08e7
Alexandre Mély <alexandre.mely@gmail.com>
2015-05-27 23:40:46 +0200

    dynamic config server settings

---
+++ config.old.php
@@ -0,0 +1,13 @@
+<?php
+$config = array_merge ($config, array (
+	'admins' => 'alexandre.mely@gmail.com,angenoir.diableblanc@gmail.com',
+	'client_id' => '320030776284-01dr10ur7ni4opbjgum9g266vrn29u4f.apps.googleusercontent.com',
+	'client_secret' => 'nHnWoauT2Ri51upqJOzKmKi0',
+	'defaultvisibility' => 'private',
+	'notif_email' => 'alexandre.mely@gmail.com',
+	'notif_from' => 'MyPhotos',
+	'notif_password' => 'codegmail2',
+	'admin_mode' => true,
+	'photopath' => '/Users/alex/Pictures/'
+));
+?>
